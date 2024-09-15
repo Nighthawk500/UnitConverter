807349b3258fae43e539a3ff1a1e8bae9423afa0
Nighthawk500 <jafm0108@gmail.com>
2024-09-15 18:39:30 -0400

    Finish Conversion class

--- app/src/main/java/com/isys221/unitconverter/Conversion.java
+++ app/src/main/java/com/isys221/unitconverter/Conversion.java
@@ -1,6 +1,17 @@
 package com.isys221.unitconverter;
 
 public class Conversion {
+    //Conversion rates
+    private final double dollarRate = 1.11;
+    private final double mileRate = 0.62;
 
-    private final double dollar = 1.11;
+    //Method to convert Euros to Dollars
+    public double convertEurosToDollars(double euros) {
+        return euros * dollarRate;
+    }
+
+    //Method to convert Km to Miles
+    public double convertKmToMiles(double km) {
+        return km * mileRate;
+    }
 }

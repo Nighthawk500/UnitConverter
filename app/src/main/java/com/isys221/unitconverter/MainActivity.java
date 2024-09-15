df71cb813133654b928745eb126c9bb95490caea
Nighthawk500 <jafm0108@gmail.com>
2024-09-15 19:43:14 -0400

    setting up controller

--- app/src/main/java/com/isys221/unitconverter/MainActivity.java
+++ app/src/main/java/com/isys221/unitconverter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.isys221.unitconverter;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     //  Individual Programming Assignment #2 - Unit Converter App
     //  Due: 9/15/2024
 
+    //Declaring variables for UI components
+    private EditText enterEurosEditText, dollarResultEditText, enterKmEditText, milesResultEditText;
+    private Button convEuroButton, convKmButton;
+    private Conversion conversionModel;
+
+    //Initializing the UI
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +32,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Link the variables to UI elements
+        enterEurosEditText = findViewById(R.id.enter_euros_edit_text);
+        enterKmEditText = findViewById(R.id.enter_km_edit_text);
+        dollarResultEditText = findViewById(R.id.dollar_result_edit_text);
+        milesResultEditText = findViewById(R.id.miles_result_edit_text);
+        convEuroButton = findViewById(R.id.convEuroButton);
+        convKmButton = findViewById(R.id.convKmButton);
+
+        //Initialize model
+        conversionModel = new Conversion();
+
+        //Create listeners for the two buttons
+        convEuroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convertEurosToDollars();
+            }
+        });
+        convKmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convertKmtoMiles();
+            }
+        });
+
+
     }
 }

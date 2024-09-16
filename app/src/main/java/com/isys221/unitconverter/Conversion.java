6722656db8a6db8c0cdee0fe9ac7a1b39c1ddf93
Nighthawk500 <jafm0108@gmail.com>
2024-09-15 20:50:50 -0400

    Add rounding

--- app/src/main/java/com/isys221/unitconverter/Conversion.java
+++ app/src/main/java/com/isys221/unitconverter/Conversion.java
@@ -3,15 +3,21 @@ package com.isys221.unitconverter;
 public class Conversion {
     //Conversion rates
     private final double dollarRate = 1.11;
-    private final double mileRate = 0.62;
+    private final double mileRate = 0.62137119;
 
     //Method to convert Euros to Dollars
     public double convertEurosToDollars(double euros) {
-        return euros * dollarRate;
+        double dollars = euros * dollarRate;
+        //Round to 4 decimals
+        dollars = Math.round(dollars * 10000.0) / 10000.0;
+        return dollars;
     }
 
     //Method to convert Km to Miles
     public double convertKmToMiles(double km) {
-        return km * mileRate;
+        double miles = km * mileRate;
+        //Round to 4 decimals
+        miles = Math.round(miles * 10000.0) / 10000.0;
+        return miles;
     }
 }
